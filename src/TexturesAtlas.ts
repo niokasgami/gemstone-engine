47e22950344bcbcf62851e5c125f46815b61e780
niokasgami <nio.kasgami.art@gmail.com>
2020-09-24 00:19:57 -0400

    InitialCommit

---
+++ src/TexturesAtlas.ts
@@ -0,0 +1,16 @@
+import * as PIXI from "pixi.js";
+
+
+class AtlasManager
+{
+
+    constructor()
+    {
+        throw new Error("This is a static class!");
+    }
+
+    public static init()
+    {
+        this.fetchData();
+    }
+}
